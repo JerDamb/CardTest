40f916c4c9efef087ebba4343858462fd6c14e0e
JerDamb <gaufre.mr@gmail.com>
2020-10-06 17:16:26 +0200

    animation première version

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
+$(document).ready(function () {
+
+    $(".card-background_background").on("click", function () {
+        // $(".card_info").toggle("slide"); 
+
+        if ($(".card-info").css("width") == "0px") {
+            $(".card-info").animate({
+                width: "65%",
+            }, 400);
+            $(".btn i").removeClass();
+            $(".btn i").addClass("fas fa-minus");
+        } else {
+            $(".card-info").animate({
+                width: "0px",
+            }, 400);
+            $(".btn i").removeClass();
+            $(".btn i").addClass("fas fa-plus");
+        }
+
+        // $(".card_info").hide("slide", { direction: "left" }, 1000);
+        //$(".card_info").show("slide", { direction: "left" }, 400);
+        // $(".btn").css({ 'transform' : 'rotate(180deg)' });
+    });
+});
 
 
-
-document.getElementById("card-background_background").addEventListener("click", function(){
+/* document.getElementById("card-background_background").addEventListener("click", function(){
 
     var container = document.getElementById("card-info");
 
     if(container.style.display=="none"){
         container.style.display="block";
-        container.style.Transition = 'opacity 1s';
     }else{
         container.style.display="none";
     }
@@ -15,9 +37,7 @@ document.getElementById("card-background_background").addEventListener("click", 
 
 
 /*const imgclick = document.querySelectorAll('.card')
-
 for (var i=0; i < imgclick.length; i++){
     imgclick[i].addEventListener('click', function(e){
-
     })
 }*/
